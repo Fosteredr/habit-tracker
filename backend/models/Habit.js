fc7e5d6a73f502da1d20cada4d95217edaddf3b9
Fosteredr <obcomfy142@gmail.com>
2026-05-26 09:46:12 +0300

    Update Habit.js

--- backend/models/Habit.js
+++ backend/models/Habit.js
@@ -10,15 +10,18 @@ const HabitSchema = new mongoose.Schema({
   title: { 
     type: String, 
     required: true // Назва звички (наприклад, "Ранкова медитація")
+    trim: true,
+    maxlength: 120
   },
   frequency: { 
     type: String, 
     default: 'щоденно' 
   },
   // Масив рядків, де зберігатимуться дати виконання у форматі YYYY-MM-DD
-  history: [{ 
-    type: String 
-  }]
+ history: {
+  type: [String],
+  default: []
+}
 }, { 
   timestamps: true 
 });
